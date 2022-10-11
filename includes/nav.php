17141407dccff03b1ca80c56d0decf63c046a312
CalD2 <95912602+CalD2@users.noreply.github.com>
2022-10-11 16:49:36 +0200

    correction user

--- includes/nav.php
+++ includes/nav.php
@@ -1,6 +1,6 @@
 <nav>
 	<p>Bienvenue <?php echo $_SESSION['prenom']; ?> <?php echo $_SESSION['nom']; ?></p>
-	<p>Membre</p>
+	<p><?php echo $_SESSION['statut']; ?></p>
 	<br>
 	<p><a href="index.php">Profil</a></p>
 	<p><a href="signalement.php">Signalement</a></p>

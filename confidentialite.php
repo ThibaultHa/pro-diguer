db770629428a00229deff436cc72c66a269c8bc5
CalD2 <95912602+CalD2@users.noreply.github.com>
2022-10-12 11:58:22 +0200

    confidentialite

--- confidentialite.php
+++ confidentialite.php
@@ -24,12 +24,19 @@ include 'traitement/userbdd.php';
         <form>
             <p>
                 <?php
-                    $sql = "SELECT share_coach FROM user WHERE user.id = ".$_SESSION['iduser'];
-                    $db->query($sql);
-                if (isset($result)) {
-                    if ($result == true) echo '<input type="checkbox" id="shareData" name="shareData" checked>';
-                    else echo '<input type="checkbox" id="shareData" name="shareData">';
-                    }
+				
+				// On récupère l'autorisation de l'utilisateur si ces données peuvent être consulté par un coath ou non
+                $sql = "SELECT share_coach FROM profil WHERE iduser = ".$_SESSION['iduser'];
+                $bdd = $db->prepare($sql);
+				$bdd->execute();
+				$result = $bdd->fetch();
+                if (isset($result)) 
+				{
+                    if ($result) 
+						echo '<input type="checkbox" id="shareData" name="shareData" checked>';
+                    else 
+						echo '<input type="checkbox" id="shareData" name="shareData">';
+                }
                 ?>
                 <label for="shareData">J'autorise à partager mes données avec mon coach carrière</label>
             </p>
